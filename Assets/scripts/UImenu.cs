9bc1c0dc333adfddd84b343cda7cc9a2bb5ff341
uttkarsh21 <narayan.u@husky.neu.edu>
2019-01-10 18:49:39 -0500

    in game menu

---
+++ Assets/scripts/UImenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImenu : MonoBehaviour
+{
+    [SerializeField]
+    Animator thisAnim;
+
+    private void Awake()
+    {
+        //thisAnim.SetInteger("menuState", 2);
+    }
+
+    public void menu_on()
+    {
+        if (thisAnim.GetInteger("menuState") == 1)
+        {
+            thisAnim.SetInteger("menuState", 2);
+        }
+        else
+        {
+            thisAnim.SetInteger("menuState", 1);
+        }
+    }
+}

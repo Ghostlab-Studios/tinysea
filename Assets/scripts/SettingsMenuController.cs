eb3b039d4369ee0ce6eee1dbd49fe0dc0af2c7c4
myersca2024 <myers.ca@northeastern.edu>
2022-07-08 17:01:03 -0400

    Added settings menu UI animations

---
+++ Assets/scripts/SettingsMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenuController : MonoBehaviour {
+
+    public Animator UIAnimator;
+
+    private bool isOpen = false;
+    
+	public void MenuPressed()
+    {
+        if (isOpen)
+        {
+            UIAnimator.SetTrigger("CloseMenu");
+            isOpen = false;
+        }
+        else
+        {
+            UIAnimator.SetTrigger("OpenMenu");
+            isOpen = true;
+        }
+    }
+}

ecdde4057c40407f82d214ccbf11b66b524d7520
Shripad Agashe <shripad.agashe.business@gmail.com>
2025-12-08 17:26:34 -0500

    Update SpeciesDatabase.cs

--- Assets/scripts/Simulation/SpeciesDatabase.cs
+++ Assets/scripts/Simulation/SpeciesDatabase.cs
@@ -71,7 +71,8 @@ public class SpeciesDatabase : ScriptableObject
 {
     public List<SpeciesData> speciesList = new List<SpeciesData>();
 
-    private int defaultSpeciesCount = 2;
+    private int defaultT1SpeciesCount = 4;
+    private int defaultT2SpeciesCount = 1;
 
     // Quick lookup by enum
     public SpeciesData GetSpecies(SpeciesName name, SpeciesVariant variant)
@@ -101,27 +102,27 @@ public class SpeciesDatabase : ScriptableObject
         // Note: Icons need to be assigned manually after population
 
         // Row 0: Cyplo Arctic
-        AddSpecies(0, SpeciesName.Sheplik, SpeciesVariant.Arctic, 1, defaultSpeciesCount, 0, 2, 0.3f, 0.02f, 1, 30,
+        AddSpecies(0, SpeciesName.Sheplik, SpeciesVariant.Arctic, 1, defaultT2SpeciesCount, 0, 2, 0.3f, 0.02f, 1, 30,
                   0, 5, 2, 3, 2, "Narrow", "Fast", 283.15f, 3564, 7088, 17554, 278.15f, 288.15f);
 
         // Row 1: Cyplo Tropical  
-        AddSpecies(1, SpeciesName.Sheplik, SpeciesVariant.Tropical, 1, defaultSpeciesCount, 0, 2, 0.3f, 0.02f, 1, 30,
+        AddSpecies(1, SpeciesName.Sheplik, SpeciesVariant.Tropical, 1, defaultT2SpeciesCount, 0, 2, 0.3f, 0.02f, 1, 30,
                   0, 5, 2, 3, 2, "Narrow", "Fast", 303.15f, 3564, 7088, 17554, 298.15f, 308.15f);
 
         // Row 2: Cyplo Common
-        AddSpecies(2, SpeciesName.Sheplik, SpeciesVariant.Common, 1, defaultSpeciesCount, 0, 1.5f, 0.3f, 0.02f, 1, 20,
+        AddSpecies(2, SpeciesName.Sheplik, SpeciesVariant.Common, 1, defaultT2SpeciesCount, 0, 1.5f, 0.3f, 0.02f, 1, 20,
                   0, 3, 2, 3, 4, "Broad", "Medium", 293.15f, 4564, 8088, 21554, 283.15f, 303.15f);
 
         // Row 3: Hexapod Arctic
-        AddSpecies(3, SpeciesName.Hexapod, SpeciesVariant.Arctic, 0, defaultSpeciesCount, 0, 1.5f, 0.3f, 0.02f, 1, 20,
+        AddSpecies(3, SpeciesName.Hexapod, SpeciesVariant.Arctic, 0, defaultT1SpeciesCount, 0, 1.5f, 0.3f, 0.02f, 1, 20,
                   0, 3, 2, 3, 2, "Narrow", "Medium", 283.15f, 3564, 7088, 17554, 278.15f, 288.15f);
 
         // Row 4: Hexapod Tropical
-        AddSpecies(4, SpeciesName.Hexapod, SpeciesVariant.Tropical, 0, defaultSpeciesCount, 0, 1.5f, 0.3f, 0.02f, 1, 20,
+        AddSpecies(4, SpeciesName.Hexapod, SpeciesVariant.Tropical, 0, defaultT1SpeciesCount, 0, 1.5f, 0.3f, 0.02f, 1, 20,
                   0, 3, 2, 3, 2, "Narrow", "Medium", 303.15f, 3564, 7088, 17554, 298.15f, 308.15f);
 
         // Row 5: Hexapod Common
-        AddSpecies(5, SpeciesName.Hexapod, SpeciesVariant.Common, 0, defaultSpeciesCount, 0, 1, 0.3f, 0.02f, 1, 15,
+        AddSpecies(5, SpeciesName.Hexapod, SpeciesVariant.Common, 0, defaultT1SpeciesCount, 0, 1, 0.3f, 0.02f, 1, 15,
                   0, 1, 2, 3, 4, "Broad", "Slow", 293.15f, 4564, 8088, 21554, 283.15f, 303.15f);
 
         EditorUtility.SetDirty(this);
